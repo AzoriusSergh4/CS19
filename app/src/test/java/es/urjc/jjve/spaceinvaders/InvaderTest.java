a0234845079eb5a24e5973bd2b305daf736d8b22
AzoriusSergh <sergiolidi@gmail.com>
2019-02-18 13:44:48 +0100

    Tests added

--- app/src/test/java/es/urjc/jjve/spaceinvaders/InvaderTest.java
+++ app/src/test/java/es/urjc/jjve/spaceinvaders/InvaderTest.java
@@ -36,6 +36,5 @@ public class InvaderTest {
             }
         }
         assertTrue(tp);
-
     }
 }
--- app/src/test/java/es/urjc/jjve/spaceinvaders/ShipTest.java
+++ app/src/test/java/es/urjc/jjve/spaceinvaders/ShipTest.java
@@ -46,7 +46,6 @@ public class ShipTest {
         boolean tpConfirmed = (oldX != newX) || (oldY !=newY);
 
         assertTrue(tpConfirmed);
-
         
     }
 }
